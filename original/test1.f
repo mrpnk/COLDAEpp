28fd7076064989a5b32b51f54f4833bf6950c787
Simon Barton <barton@ph1.uni-koeln.de>
2022-03-11 12:29:25 +0100

    Restructured

---
+++ original/test1.f
@@ -0,0 +1,71 @@
+      ! z'(x)=x
+      ! z(0)=1    
+      subroutine fsub(x,z,y,f)
+            double precision x, z(1), y(0), f(1)
+            f(1)=x ! z'(x)
+      end
+      subroutine dfsub(x,z,y,df)
+            double precision x, z(1), y(0), df(1,1)
+            df(1,1)=0 ! dz'/dz
+      end
+
+      subroutine gsub(i,z,g)
+            integer i
+            double precision z(1), g
+            g=z(1)-1 ! 0=z(0)-1
+      end
+      subroutine dgsub(i,z,dg)
+            integer i
+            double precision z(1), dg(1)
+            dg(1)=1 ! d/dz (z(0)-1)
+      end
+
+      program test
+      implicit none
+            integer ncomp, ny, orders(1),i,j
+            integer ipar(12), ispace(10000), ltol(1), iflag
+            double precision left,right,fspace(10000),x,z(1),y(0)
+            double precision sides(1), tol(1), fixpnt(0)
+            external fsub,dfsub,gsub,dgsub
+
+            character(len = 32) :: outFormat = '(32(e19.12,1x))'
+
+            ncomp=1;
+            ny=0;
+            orders(1) = 1;
+            left = 0.0;
+            right = 1.0;
+            sides(1) = 0.0; ! first bc is on the left
+            ltol(1) = 1 ! 1st tolerance is for z_1
+            tol(1) = 0.1; ! 1st tolerance
+
+            ipar(1)=0 ! problem is linear
+            ipar(2)=0 ! auto num of collocation points
+            ipar(3)=0 ! auto num of subintervals
+            ipar(4)=1 ! number of tolerances
+            ipar(5)=10000 ! dim of fspace
+            ipar(6)=10000 ! dim of ispace
+            ipar(7)=-1 ! full printout 
+            ipar(8)=0 ! generate new mesh
+            ipar(9)=0 ! no guess provided
+            ipar(10)=0 ! problem is regular
+            ipar(11)=0 ! no fixed points
+            ipar(12)=0 ! dae index, ignored
+
+            call coldae(ncomp,ny,orders,left,right,sides,
+     .                 ipar,ltol,tol,fixpnt,ispace,fspace,iflag,
+     .                 fsub,dfsub,gsub,dgsub,0)
+
+            write(*,*) "iflag = ", iflag
+
+            if(iflag == 1) then
+                  open(100,file=trim("result1.txt"))
+                  do i = 1, ispace(1)+1 
+                        x = fspace(i)
+                        call appsln(x,z,y,fspace,ispace)
+                        write(100,outFormat) x,z(1)
+                  enddo
+                  close(100)
+            endif                                     
+
+      end
---
+++ src/test.hpp
@@ -0,0 +1,128 @@
+#pragma once
+#include "ColDAEpp.hpp"
+
+
+// f'(x) = x, f(0)=1
+struct sys1 {
+    systemParams params;
+    sys1() {
+        params.ncomp = 1;
+        params.ny = 0;
+        params.orders = { 1 };
+        params.left = 0;
+        params.right = 1;
+        params.bcpoints = { 0 };
+        params.isNonLinear = false;
+        params.regularity = regularControl::regular;
+    }
+
+    static void fsub(double x, const double z[1], const double y[0], double f[1]) {
+        f[0] = x;
+    }
+    static void dfsub(double x, const double z[1], const double y[0], double df[1*1]) {
+        df[0 + 0*1] = 0;
+    }
+    static void gsub(int i, const double z[1], double& g) {
+        g = z[0] - 1;
+    }
+    static void dgsub(int i, const double z[1], double dg[1*1]) {
+        dg[0] = 1;
+    }
+};
+
+
+// f''(x) = sin(0.6*f'(x)) + x, f(0)=1, f(1)=-0.1
+struct sys2 {
+    systemParams params;
+    sys2() {
+        params.ncomp = 2;
+        params.ny = 0;
+        params.orders = { 1,1 };
+        params.left = 0;
+        params.right = 1;
+        params.bcpoints = { 0, 1 };
+        params.isNonLinear = true;
+        params.regularity = regularControl::regular;
+        params.index = indexControl::automatic; // not used
+    }
+
+    static void fsub(double x, const double z[2], const double y[], double f[2]) {
+        f[0] = z[1]; // z'(x)
+        f[1] = sin(0.6 * z[1]) + x; // z''(x)
+    }
+    static void dfsub(double x, const double z[2], const double y[], double df[2*2]) {
+        df[0 + 0*2] = 0; // dz'/dz
+        df[0 + 1*2] = 1; // dz'/dz'
+        df[1 + 0*2] = 0; // dz'' / dz
+        df[1 + 1*2] = 0.6 * cos(0.6 * z[1]); // dz'' / dz'
+    }
+    static void gsub(int i, const double z[2], double& g) {
+        if (i == 1)
+            g = z[0] - 1; // 0=z(0)-1  left
+        else if (i == 2)
+            g = z[1] + 0.1; // 0=z(0)+0.1  right
+        else throw(std::invalid_argument("gsub: index out of range."));
+    }
+    static void dgsub(int i, const double z[2], double dg[2]) {
+        if (i == 1) {
+            dg[0] = 1;  // d/dz  g_left
+            dg[1] = 0;  // d/dz' g_left
+        }
+        else if (i == 2) {
+            dg[0] = 0;  // d/dz  g_right
+            dg[1] = 1;  // d/dz' g_right
+        }
+        else throw(std::invalid_argument("dgsub: index out of range."));
+    }
+};
+
+
+// f1''(x) = f2+f1', 0 = f2 + f1*f1' - x, f1(0)=1, f1'(0)=0.1
+struct sys3 {
+    systemParams params;
+    sys3() {
+        params.ncomp = 1;
+        params.ny = 1;
+        params.orders = { 2 };
+        params.left = 0;
+        params.right = 5;
+        params.bcpoints = { 0, 5 };
+        params.isNonLinear = true;
+        params.regularity = regularControl::regular;
+        params.index = indexControl::automatic;
+    }
+
+    static void fsub(double x, double const z[2], double const y[1], double f[2]) {
+        f[0] = y[0] + z[1]; // f_1 = f1''(x)
+        f[1] = y[0] + z[0] * z[1] - x; // f_2 = 0 = algebraic
+    }
+    static void dfsub(double x, double const z[2], double const y[1], double df[6]) {
+        const int ms = 2;
+        df[0 + 0 * ms] = 0; // df_1 / df1
+        df[0 + 1 * ms] = 1; // df_1 / df1'
+        df[0 + 2 * ms] = 1; // df_1 / df2
+
+        df[1 + 0 * ms] = z[1]; // df_2 / df1
+        df[1 + 1 * ms] = z[0]; // df_2 / df1'
+        df[1 + 2 * ms] = 1;    // df_2 / df2
+    }
+    static void gsub(int i, double const z[2], double& g) {
+        if (i == 1)
+            g = z[0] + z[1] + 0.15;  // g_1 = 0 = z(0)-1   left
+        else if (i == 2)
+            g = z[0] - 6.5;          // g_2 = 0 = z(0)-1   right
+        else throw(std::invalid_argument("gsub: index out of range."));
+    }
+    static void dgsub(int i, double const z[2], double dg[2]) {
+        if (i == 1) {
+            dg[0] = 1; // dg_1 / df1
+            dg[1] = 1; // dg_1 / df1'
+        }
+        else if (i == 2) {
+            dg[0] = 1;  // dg_2/df1
+            dg[1] = 0;  // dg_2/df1'
+        }
+        else throw(std::invalid_argument("dgsub: index out of range."));
+    }
+};
+
